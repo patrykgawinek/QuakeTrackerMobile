35415437b5547433c6269f9a3cb0be8bc9ec37c3
brent_joosens <brent.joosens@student.ap.be>
2021-12-20 11:21:39 +0100

    Styling from the FoundFeatures Card

--- screens/FoundFeatures.tsx
+++ screens/FoundFeatures.tsx
@@ -2,7 +2,14 @@ import { useNavigation } from "@react-navigation/native";
 import { StackNavigationProp } from "@react-navigation/stack";
 import axios, { AxiosResponse } from "axios";
 import React, { useContext, useEffect, useState } from "react";
-import { ActivityIndicator, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import {
+  ActivityIndicator,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+  ViewStyle,
+} from "react-native";
 import { FlatList, ScrollView } from "react-native-gesture-handler";
 import { FoundFeaturesContext } from "../context/FoundFeaturesContext";
 import { AlertLevel } from "../types";
@@ -10,16 +17,26 @@ import { AlertLevel } from "../types";
 interface ListItemProps {
   feature: any;
   handleSelectEarthquake: (id: string) => void;
+  style: ViewStyle;
 }
 
-const ListItem = ({ handleSelectEarthquake, feature }: ListItemProps) => {
+const ListItem = ({
+  handleSelectEarthquake,
+  feature,
+  style,
+}: ListItemProps) => {
   return (
-    <TouchableOpacity onPress={() => handleSelectEarthquake(feature.id)}>
-      <Text>Place: {feature.properties.place}</Text>
-      <Text>
+    <TouchableOpacity
+      onPress={() => handleSelectEarthquake(feature.id)}
+      style={style}
+    >
+      <Text style={styles.cardTitle}>Place: {feature.properties.place}</Text>
+      <Text style={styles.cardText}>
         Magnitude(type {feature.properties.magType}): {feature.properties.mag}
       </Text>
-      <Text>Alert level: {feature.properties.alert}</Text>
+      <Text style={styles.cardText}>
+        Alert level: {feature.properties.alert}
+      </Text>
     </TouchableOpacity>
   );
 };
@@ -51,7 +68,9 @@ const FoundFeatures = () => {
         maxmagnitude: magnitudeRange.maximum,
         minmagnitude: magnitudeRange.minimum,
         alertlevel:
-          alertLevel === AlertLevel.All ? "" : AlertLevel[alertLevel].toString().toLowerCase(),
+          alertLevel === AlertLevel.All
+            ? ""
+            : AlertLevel[alertLevel].toString().toLowerCase(),
       },
     });
     setEarthquakeData(response.data.features);
@@ -69,14 +88,22 @@ const FoundFeatures = () => {
   return (
     <View>
       {loading ? (
-        <ActivityIndicator color={"blue"} size={"large"} style={styles.spinner} />
+        <ActivityIndicator
+          color={"blue"}
+          size={"large"}
+          style={styles.spinner}
+        />
       ) : (
         <FlatList
           refreshing={loading}
           onRefresh={refresh}
           data={earthquakeData}
           renderItem={({ item }) => (
-            <ListItem feature={item} handleSelectEarthquake={handleSelectEarthquake} />
+            <ListItem
+              style={styles.card}
+              feature={item}
+              handleSelectEarthquake={handleSelectEarthquake}
+            />
           )}
         ></FlatList>
       )}
@@ -88,14 +115,27 @@ const styles = StyleSheet.create({
   spinner: {
     marginTop: 20,
   },
-  featureContainer: {
+  card: {
     alignSelf: "stretch",
     margin: 10,
     padding: 5,
-    borderBottomColor: "black",
-    borderBottomWidth: 1,
-    borderTopColor: "black",
-    borderTopWidth: 1,
+    backgroundColor: "#fff",
+    borderRadius: 5,
+    shadowOffset: {
+      width: 20,
+      height: -20,
+    },
+    shadowOpacity: 0.75,
+    shadowRadius: 5,
+    shadowColor: "#000",
+    elevation: 3,
+  },
+  cardTitle: {
+    fontWeight: "bold",
+    fontSize: 16,
+  },
+  cardText: {
+    fontSize: 16,
   },
 });
 
